7a05c4da0c44cf6a4b2b4d64b0de2c2f41d33571
Sarah Gonsalves <sarahgonsalves223@gmail.com>
2019-08-21 23:51:11 -0700

    Easy palindrome check

---
+++ Easy/125_easy_valid-palindrome.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+# https://leetcode.com/problems/valid-palindrome/description/
+#
+# algorithms
+# Easy (32.00%)
+# Total Accepted:    392.9K
+# Total Submissions: 1.2M
+# Testcase Example:  '"A man, a plan, a canal: Panama"'
+#
+# Given a string, determine if it is a palindrome, considering only
+# alphanumeric characters and ignoring cases.
+# 
+# Note: For the purpose of this problem, we define empty string as valid
+# palindrome.
+# 
+# Example 1:
+# 
+# 
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: "race a car"
+# Output: false
+# 
+# 
+#
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        s = s.lower()
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while j > i and not s[j].isalnum():
+                j -= 1
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+        
